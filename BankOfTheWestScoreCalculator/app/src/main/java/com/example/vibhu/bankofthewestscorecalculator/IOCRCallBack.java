940146bd788cfa06c2160a1dd58409a52d63f8f9
Vibhu Varshney <varshney.vibhu72@gmail.com>
2019-10-05 10:14:39 -0700

    App created with login

---
+++ BankOfTheWestScoreCalculator/app/src/main/java/com/example/vibhu/bankofthewestscorecalculator/IOCRCallBack.java
@@ -0,0 +1,10 @@
+package com.example.vibhu.bankofthewestscorecalculator;
+
+/**
+ * @ Firstcry
+ * Created by suhasbachewar on 18/5/16.
+ */
+public interface IOCRCallBack {
+
+    void getOCRCallBackResult(String response);
+}
